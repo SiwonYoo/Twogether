2c48650fbb29e42ec99613fee24dec94833e51be
koo-rogie <rn5184@naver.com>
2025-07-08 16:59:54 +0900

    ✨feat: Add auto-push feature after forking repository

---
+++ output/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <>
+      <h1>Final-Project-Sample</h1>
+      <p>프로젝트 메인사이트입니다</p>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <>
       <h1>Final-Project-Sample</h1>
       <p>프로젝트 메인사이트입니다</p>
+      <p>프로젝트 자동배포 테스트입니다.</p>
     </>
   );
 }
