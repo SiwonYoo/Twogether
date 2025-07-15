8e31f9d420b4b11f0bc5e926449220f3f4f9cd35
koo-rogie <rn5184@naver.com>
2025-07-15 15:46:59 +0900

    🔀merge: Merge develop branch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,11 @@
 import './globals.css';
+import localFont from 'next/font/local';
+
+const pretendard = localFont({
+  src: '../../public/font/PretendardVariable.ttf',
+  display: 'swap', // 폰트 로딩
+  weight: '45 920',
+});
 
 export default function RootLayout({
   children,
@@ -7,8 +14,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className="flex items-center justify-center min-h-screen px-4 bg-white">
-        <div className="w-full min-w-[400px] max-w-[768px] bg-white">{children}</div>
+      <body className={`flex items-center justify-center min-h-screen bg-white px-4 ${pretendard.className}`}>
+        <div className="relative w-full min-w-[400px] max-w-[768px] min-h-dvh bg-white">
+          {/* <Header /> */}
+          {children}
+          {/* <Footer /> */}
+          {/* <Navigation /> */}
+        </div>
       </body>
     </html>
   );
