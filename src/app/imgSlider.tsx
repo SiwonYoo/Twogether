3d6a26899aa94f3c0b8aaf9abaf2f22a0f018c00
koo-rogie <rn5184@naver.com>
2025-07-16 14:48:54 +0900

    ♻️ refactor: clean up ImgSlider component and remove unused onSlideChange log

--- src/app/imgSlider.tsx
+++ src/app/imgSlider.tsx
@@ -37,7 +37,7 @@ export default function ImgSlider() {
           loop={true} // 슬라이드 루프
           spaceBetween={0} // 슬라이스 사이 간격
           slidesPerView={1} // 보여질 슬라이스 수
-          onSlideChange={() => console.log('slide change')}
+          // onSlideChange={() => console.log('slide change')}
           autoplay={{
             delay: 3000,
             disableOnInteraction: false, // 사용자 상호작용시 슬라이더 일시 정지 비활성
