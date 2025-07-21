b4f7726889731c6172c460b1212a2011b305b212
koo-rogie <rn5184@naver.com>
2025-07-21 15:47:40 +0900

    ♻️ refactor: update ShoppingCartAdd to use existing Button and SubDialog components

--- src/app/shop/[productType]/[id]/ShoppingCartAdd.tsx
+++ src/app/shop/[productType]/[id]/ShoppingCartAdd.tsx
@@ -1,13 +1,26 @@
+'use client';
 import Button from '@/components/common/Button';
+import Dialog from '@/components/common/SubDialog';
 import { ShoppingBag } from 'lucide-react';
-import Link from 'next/link';
+import { useState } from 'react';
 
 export default function ShoppingCartAdd() {
+  const [isOpen, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(!isOpen);
+    console.log('클릭함');
+  };
+
   return (
     <>
-      <button className="flex justify-center items-center border border-(--color-primary) text-center w-1/4 px-6 py-2 bg-(--color-white)">
+      <Button
+        onClick={handleClick}
+        className="flex justify-center items-center border border-(--color-primary) text-center w-1/4 px-6 py-2 bg-(--color-white)"
+      >
         <ShoppingBag />
-      </button>
+      </Button>
+      <Dialog isOpen={isOpen} setOpen={setOpen} />
     </>
   );
 }
