28e4921c7daf65d93cc296441cfcd17fc9f354d8
koo-rogie <rn5184@naver.com>
2025-07-21 17:28:44 +0900

    ♻️ refactor: extract component for cloud environment in Next.js

--- src/app/shop/[productType]/[id]/page.tsx
+++ src/app/shop/[productType]/[id]/page.tsx
@@ -1,11 +1,6 @@
-import ProductTypeIdItme from '@/app/shop/[productType]/[id]/ProductTypeIdItme';
-import ShoppingCartAdd from '@/app/shop/[productType]/[id]/ShoppingCartAdd';
-import Button from '@/components/common/Button';
-import DropDown from '@/components/common/DropDown';
+import ProductSelect from '@/app/shop/[productType]/[id]/productSelect';
 import ImgSlider from '@/components/common/imgSlider';
-import LikeButton from '@/components/product/LikeButton';
 import { Metadata } from 'next';
-import Link from 'next/link';
 
 interface ProductDetailPageProps {
   params: Promise<{
@@ -33,6 +28,12 @@ export default function ProductDetailPage() {
       title: '코튼캔디 순면 반팔 상하의 세트(남녀공용)',
       pries: 12000,
       sale: 9000,
+      size: [
+        { value: 'FREE', text: 'FREE' },
+        { value: 'S', text: 'S' },
+        { value: 'M', text: 'M' },
+        { value: 'L', text: 'L' },
+      ],
     },
   ];
 
@@ -55,37 +56,7 @@ export default function ProductDetailPage() {
                 <span>배송금: </span>
                 <span>3,000원{' (50,000원 이상 구매시 무료 배송)'}</span>
               </p>
-              <div className="border my-6 p-4 border-(--color-gray-350)">
-                <DropDown
-                  id="size"
-                  label="사이즈"
-                  items={[
-                    { value: 'free', text: 'free' },
-                    { value: 's', text: 's' },
-                    { value: 'M', text: 'M' },
-                    { value: 'L', text: 'L' },
-                    { value: 'XL', text: 'XL' },
-                    { value: 'XXL', text: 'XXL' },
-                  ]}
-                  placeHolder="사이즈를 선택해주세요"
-                />
-              </div>
-              <div className=" bg-(--color-gray-250) p-4">
-                <ProductTypeIdItme itme={itme} />
-                <div className="flex justify-between items-center gap-2">
-                  <div className="flex justify-center items-center border border-(--color-primary) text-center w-1/4  px-6 py-2 bg-(--color-white) relative">
-                    <LikeButton />
-                  </div>
-                  <ShoppingCartAdd />
-                  <div className="w-2/3">
-                    <Link href="/order">
-                      <Button shape="square" size="lg">
-                        구매하기
-                      </Button>
-                    </Link>
-                  </div>
-                </div>
-              </div>
+              <ProductSelect itme={itme} />
             </div>
           );
         })}
