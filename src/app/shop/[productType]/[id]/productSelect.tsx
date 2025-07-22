13d51d16692ddc4160e0e4ce08ad7f1b6ae8959f
koo-rogie <rn5184@naver.com>
2025-07-22 09:12:06 +0900

    ✏️ typo: Fix import path for ProductTypeIdItem component

--- src/app/shop/[productType]/[id]/productSelect.tsx
+++ src/app/shop/[productType]/[id]/productSelect.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import ProductTypeIdItem from '@/app/shop/[productType]/[id]/productTypeIdItem';
+import ProductTypeIdItem from '@/app/shop/[productType]/[id]/ProductTypeIdItem';
 import ShoppingCartAdd from '@/app/shop/[productType]/[id]/ShoppingCartAdd';
 import Button from '@/components/common/Button';
 import DropDown from '@/components/common/DropDown';
