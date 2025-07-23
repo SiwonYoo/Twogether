9810b303a1fd1d427c749e148d50b1579baaddec
apppiel <94946906+apppiel@users.noreply.github.com>
2025-07-23 14:34:53 +0900

    ✨feat: Add post list view page

--- src/app/community/[boardType]/page.tsx
+++ src/app/community/[boardType]/page.tsx
@@ -1,6 +1,9 @@
 import EventList from '@/app/community/EventList';
 import NoticeList from '@/app/community/NoticeList';
+import Input from '@/components/common/Input';
+import { getPosts } from '@/data/functions/post';
 import { Metadata } from 'next';
+import Link from 'next/link';
 
 export async function generateMetadata({ params }: ListPageProps): Promise<Metadata> {
   const { boardType } = await params;
@@ -26,11 +29,26 @@ export interface ListPageProps {
 
 export default async function CommunityPage({ params }: ListPageProps) {
   const { boardType } = await params;
+  const res = await getPosts(boardType);
 
   return (
     <>
-      {boardType === 'event' && <EventList />}
-      {boardType === 'notice' && <NoticeList />}
+      <ul className="mb-25">
+        {/* 공지 부분은 하드코딩 */}
+
+        {boardType === 'notice' &&
+          (res.ok ? (
+            res.item.map((post, i) => <NoticeList key={i} post={post} boardType={boardType} />)
+          ) : (
+            <p>{res.message}</p>
+          ))}
+        {boardType === 'event' &&
+          (res.ok ? (
+            res.item.map((post, i) => <EventList key={i} post={post} boardType={boardType} />)
+          ) : (
+            <p>{res.message}</p>
+          ))}
+      </ul>
     </>
   );
 }
