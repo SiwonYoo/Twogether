5ad97d5adda586441b1e83fdd9a299ebcb037290
apppiel <94946906+apppiel@users.noreply.github.com>
2025-07-25 00:12:15 +0900

    ♻️refactor: Post.ts deletePost _id instead of id for clarity

--- src/data/actions/post.ts
+++ src/data/actions/post.ts
@@ -103,7 +103,51 @@ export async function updatePost(state: ApiRes<Post> | null, formData: FormData)
   if (data.ok) {
     revalidateTag(`posts/${_id}`); // 게시글 상세 페이지 갱신
     revalidateTag(`posts?type=${type}`); // 게시글 목록 페이지 갱신
-    redirect(`/${type}/${_id}`);
+    redirect(`/my-page/${type}/${_id}`);
+  } else {
+    return data;
+  }
+}
+
+/**
+ * 게시글을 삭제하는 함수
+ * @param {ApiRes<Post> | null} state - 이전 상태(사용하지 않음)
+ * @param {FormData} formData - 삭제할 게시글 정보를 담은 FormData 객체
+ * @returns {Promise<ApiRes<Post>>} - 삭제 결과 응답 객체
+ * @throws {Error} - 네트워크 오류 발생 시
+ * @description
+ * 게시글을 삭제하고, 성공 시 해당 게시판 목록 페이지로 리다이렉트합니다.
+ * 실패 시 에러 메시지를 반환합니다.
+ */
+export async function deletePost(state: ApiRes<Post> | null, formData: FormData): ApiResPromise<Post> {
+  const id = formData.get('id');
+  const type = formData.get('type');
+  const accessToken = formData.get('accessToken');
+
+  let res: Response;
+  let data: ApiRes<{ ok: 0 | 1 }>;
+
+  try {
+    res = await fetch(`${API_URL}/posts/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Client-Id': CLIENT_ID,
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    data = await res.json();
+  } catch (error) {
+    // 네트워크 오류 처리
+    console.error(error);
+    return { ok: 0, message: '일시적인 네트워크 문제가 발생했습니다.' };
+  }
+
+  if (data.ok) {
+    revalidateTag(`posts/${id}`);
+    revalidateTag(`posts?type=${type}`);
+    redirect(`/my-page/${type}`);
   } else {
     return data;
   }
