2720654181bd5be5df4e669bfa20056a0b42835b
시원 <sw622@naver.com>
2025-07-25 18:26:40 +0900

    ✨ feat: Add email duplication check during signup
    - Implemented API call to check if email is already in use
    - Added missing 'use server' directive for server actions

--- src/data/actions/user.ts
+++ src/data/actions/user.ts
@@ -1,8 +1,48 @@
+'use server';
+
 import { ApiRes, ApiResPromise, User } from '@/types';
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID || '';
 
+/**
+ * 회원가입 함수
+ */
+export async function signup(state: ApiRes<User> | null, formData: FormData): ApiResPromise<User> {
+  let res: Response;
+  let data: ApiRes<User>;
+
+  try {
+    const body = {
+      /* 관리자 추가할 경우, form 데이터로 받기 */
+      type: 'user',
+      name: formData.get('name'),
+      email: formData.get('email'),
+      password: formData.get('password'),
+      phone: formData.get('phone'),
+    };
+
+    res = await fetch(`${API_URL}/users`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Client-Id': CLIENT_ID,
+      },
+      body: JSON.stringify(body),
+    });
+
+    data = await res.json();
+  } catch (error) {
+    console.error(error);
+    return { ok: 0, message: '일시적인 네트워크 문제가 발생했습니다.' };
+  }
+
+  return data;
+}
+
+/**
+ * 로그인 함수
+ */
 export async function login(state: ApiRes<User> | null, formData: FormData): ApiResPromise<User> {
   const body = Object.fromEntries(formData.entries());
 
@@ -19,7 +59,6 @@ export async function login(state: ApiRes<User> | null, formData: FormData): Api
       body: JSON.stringify(body),
     });
     data = await res.json();
-    console.log('로그인 성공', data);
   } catch (error) {
     console.error(error);
     return { ok: 0, message: '일시적인 네트워크 문제가 발생했습니다.' };
