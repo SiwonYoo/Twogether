2a9ebe5fa31af7593284ddd4714bf6322a5f2d2a
koo-rogie <rn5184@naver.com>
2025-07-28 16:03:36 +0900

    🐳 chore: WIP - intermediate commit for product detail logic

--- src/app/like/UserLikePage.tsx
+++ src/app/like/UserLikePage.tsx
@@ -1,6 +1,5 @@
-// TODO 나중에 로그인 정보가 들어오면 로그인 확인 페이지로 변경해야함.
-import LikePageApi from '@/app/like/LikePageApi';
 import LinkButton from '@/components/common/LinkButton';
+import ProductCardItem from '@/components/product/ProductCardItem';
 import { getProducts } from '@/data/functions/shop';
 import { Metadata } from 'next';
 import { Judson } from 'next/font/google';
@@ -19,9 +18,8 @@ export const metadata: Metadata = {
   },
 };
 
-export default async function LikePage() {
+export default async function UserLikePage() {
   const data = await getProducts();
-
   // 서버연결이 어려울때 나오는 컨포넌트
   if (data.ok === 0) {
     return (
@@ -35,11 +33,28 @@ export default async function LikePage() {
     );
   }
 
+  const likedItems = data.item.filter((item) => item.extra.isLike === true);
+
   // 서버연결이 원할할때 나오는 컨포넌트
   return (
     <main className="mx-4">
       <h2 className={`font-bold text-4xl text-center ${JudsonFont.className}`}>LIKE</h2>
-      <LikePageApi item={data.item} />
+      {likedItems.length > 0 ? (
+        <ul className="grid grid-cols-2 gap-4 mt-4">
+          {likedItems.map((item) => (
+            <ProductCardItem key={item._id} data={[item]} />
+          ))}
+        </ul>
+      ) : (
+        <div className="font-bold text-center py-8 bg-(--color-gray-150) rounded-2xl my-6 p-4">
+          <p className="text-3xl mb-4">찜한 상품이 아직 없습니다.</p>
+          <p className="text-gray-500">고객님이 찜하신 상품이 이곳에 표시됩니다.</p>
+          <p className="text-gray-500 my-2">마음에 드는 상품을 찜해보세요!</p>
+          <p className="text-gray-500">상품 상세 페이지에서 하트를 누르면 찜 목록에 담을 수 있어요.</p>
+          <p className="text-gray-500 mb-4 mt-2">다양한 상품을 둘러보고 원하는 아이템을 저장해보세요.</p>
+          <LinkButton href={`/shop/shortSleeve`}>상품 둘러보기</LinkButton>
+        </div>
+      )}
     </main>
   );
 }
