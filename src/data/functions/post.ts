96772afc905a0ec79e50893c5deb322637be4a09
apppiel <94946906+apppiel@users.noreply.github.com>
2025-07-31 11:26:21 +0900

    ♻️refactor: Separate post search into new function

--- src/data/functions/post.ts
+++ src/data/functions/post.ts
@@ -5,20 +5,45 @@ const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID || '';
 
 /**
  * 게시판 타입에 해당하는 게시글 목록을 가져옵니다.
- * @param {string} boardType - 게시판 타입(예: notice, free 등)
+ * @param {string} boardType - 게시판 타입(예: notice, event 등)
+ * @returns {Promise<ApiRes<Post[]>>} - 게시글 목록 응답 객체
+ */
+export async function getPosts(boardType: string): ApiResPromise<Post[]> {
+  try {
+    const res = await fetch(`${API_URL}/posts?type=${boardType}`, {
+      headers: {
+        'Client-Id': CLIENT_ID,
+      },
+      cache: 'no-store', // force-cache에서 no-store로 바꿔서 게시글 작성 후 목록으로 돌아왔을 때 바로 보이게 가능
+      next: {
+        tags: [`posts?type=${boardType}`],
+      },
+    });
+    return res.json();
+  } catch (error) {
+    // 네트워크 오류 처리
+    console.error(error);
+    return { ok: 0, message: '일시적인 네트워크 문제로 등록에 실패했습니다.' };
+  }
+}
+
+/**
+ * 게시판 타입에 해당하는 게시글 목록을 가져옵니다.
+ * @param {string} boardType - 게시판 타입(예: notice, event 등)
  * @param {string} keyword - 검색 키워드(에: 배송, 반품, 상품, 기타 등)
  * @returns {Promise<ApiRes<Post[]>>} - 게시글 목록 응답 객체
  */
-export async function getPosts(boardType: string, keyword: string = ''): ApiResPromise<Post[]> {
+export async function getSearchPosts(boardType: string, keyword: string = ''): ApiResPromise<Post[]> {
   try {
     const query = new URLSearchParams();
 
     //keyword가 공백이 아니라면 query에 keyword 값 추가
-    if (keyword.trim() !== '') {
-      query.set('keyword', keyword);
-    }
 
-    const res = await fetch(`${API_URL}/posts?type=${boardType}&${query.toString()}`, {
+    query.set('keyword', keyword.trim());
+
+    console.log(query);
+
+    const res = await fetch(`${API_URL}/posts?type=${boardType}&keyword=${keyword}`, {
       headers: {
         'Client-Id': CLIENT_ID,
       },
