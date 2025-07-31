4eb84fc5b8b3a0547a9f91bd228961b285fe2c94
koo-rogie <rn5184@naver.com>
2025-07-31 11:48:57 +0900

    ✨feat: Replace dummy Q&A & notice data with getPosts API calls

--- src/app/shop/[productType]/[id]/QnA.tsx
+++ src/app/shop/[productType]/[id]/QnA.tsx
@@ -1,47 +1,60 @@
+import { getPosts } from '@/data/functions/post';
+import useUserStore from '@/stores/useUserStore';
+import { Post } from '@/types';
 import { ProductDetails } from '@/types/product';
 import { Judson } from 'next/font/google'; // 구글 폰트 사용
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 const JudsonFont = Judson({
   subsets: ['latin'],
   weight: '700',
 });
 
-interface userDataProp {
-  _id: number;
-  title: string;
-  user: string;
-  date: string;
-  view: number;
-}
-interface AdminDataProp {
-  _id: number;
-  title: string;
-  user: string;
-  date: string;
-  view: number;
-}
-
 export default function QnA({ productType, product }: ProductDetails) {
-  const userData: userDataProp[] = [
-    { _id: 5, title: '문의 드립니다', user: '홍길동', date: '25.08.03', view: 100 },
-    { _id: 4, title: '문의 드립니다', user: '홍길동', date: '25.08.03', view: 100 },
-    { _id: 3, title: '문의 드립니다', user: '홍길동', date: '25.08.01', view: 100 },
-    { _id: 2, title: '문의 드립니다', user: '홍길동', date: '25.08.03', view: 100 },
-    { _id: 1, title: '문의 드립니다', user: '홍길동', date: '25.08.01', view: 100 },
-  ];
+  const [NoticePage, setNoticePag] = useState<Post[]>([]);
+  const [qnaPage, setQnaPag] = useState<Post[]>([]);
+  const { user } = useUserStore();
 
-  const adminData: AdminDataProp[] = [
-    { _id: 1, title: '투게더 교환 및 반품 안내', user: '투게더', date: '25.08.01', view: 2 },
-    { _id: 2, title: '투게더 배송안내', user: '투게더', date: '25.08.01', view: 2 },
-  ];
+  useEffect(() => {
+    async function noticeApi() {
+      const res = await getPosts('notice');
+      if (res.ok === 0) {
+        return null;
+      }
+      if (res.ok === 1) {
+        setNoticePag(res.item);
+      }
+      console.log(res);
+    }
+
+    async function QnAApi() {
+      const res = await getPosts('qna');
+      if (res.ok === 0) {
+        return null;
+      }
+      if (res.ok === 1) {
+        setQnaPag(res.item);
+      }
+      console.log(res);
+    }
+    noticeApi();
+    QnAApi();
+  }, [NoticePage, qnaPage]);
+
+  // 날짜 변환
+  function formatToYYMMDD(datetime: string) {
+    const [datePart] = datetime.split(' ');
+    const [year, month, day] = datePart.split('.');
+    return `${year.slice(2)}.${month}.${day}`;
+  }
 
   return (
     <>
       <h2 className={`${JudsonFont.className} text-center font-bold text-2xl`}>Q&A</h2>
       <ul>
         {/* 공지 */}
-        {adminData.map((item) => {
+        {NoticePage.slice(0, 2).map((item) => {
           return (
             <li key={item._id} className="border-b border-(--color-gray-250) my-4">
               <Link href={`/community/notice/${item._id}`}>
@@ -50,16 +63,16 @@ export default function QnA({ productType, product }: ProductDetails) {
                   <p className="font-bold ">{item.title}</p>
                 </div>
                 <div className="flex gap-4 my-2">
-                  <p>{item.user}</p>
-                  <p>{item.date}</p>
-                  <p>조회: {item.view}</p>
+                  <p>{item.user.name}</p>
+                  <p>{formatToYYMMDD(item.createdAt)}</p>
+                  <p>조회: {item.views}</p>
                 </div>
               </Link>
             </li>
           );
         })}
 
-        {userData.map((item) => {
+        {qnaPage.map((item) => {
           return (
             <li key={item._id} className="border-b  border-(--color-gray-250) my-4">
               <Link href={`/my-page/qna/${item._id}`}>
@@ -67,9 +80,13 @@ export default function QnA({ productType, product }: ProductDetails) {
                   <p>{item.title}</p>
                 </div>
                 <div className="flex gap-4 my-2">
-                  <h3>{item.user.length > 1 ? item.user[0] + '*'.repeat(item.user.length - 1) : item.user}</h3>
-                  <p>{item.date}</p>
-                  <p>조회: {item.view}</p>
+                  <h3>
+                    {item.user.name.length > 1
+                      ? item.user.name[0] + '*'.repeat(item.user.name.length - 1)
+                      : item.user.name}
+                  </h3>
+                  <p>{formatToYYMMDD(item.createdAt)}</p>
+                  <p>조회: {item.views}</p>
                 </div>
               </Link>
             </li>
