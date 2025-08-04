8218c5016e2be963fd733881df354a68d7ff93fb
시원 <sw622@naver.com>
2025-08-04 17:41:54 +0900

    ✨ feat: Implement find-email feature by verifying name and phone number
    - add form to find user email using name and phone number
    - display masked email if user exists
    - show alert message if user is not found

---
+++ src/app/(user)/find-email/page.tsx
@@ -0,0 +1,11 @@
+import FindEmailForm from '@/app/(user)/find-email/FindEmailForm';
+
+function FindEmail() {
+  return (
+    <main className="mx-4">
+      <FindEmailForm />
+    </main>
+  );
+}
+
+export default FindEmail;
