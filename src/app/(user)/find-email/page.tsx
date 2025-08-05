21618a901dd8604d4384a410c91b51650ae912bf
시원 <sw622@naver.com>
2025-08-05 09:41:27 +0900

    ♻️ refactor: apply consistent subheader for 3-depth pages like review and order details

--- src/app/(user)/find-email/page.tsx
+++ src/app/(user)/find-email/page.tsx
@@ -1,9 +1,25 @@
 import FindEmailForm from '@/app/(user)/find-email/FindEmailForm';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '이메일 찾기 - Twogether',
+  description: 'Twogether의 이메일 찾기 페이지입니다.',
+
+  openGraph: {
+    title: '이메이 찾기 - Twogether',
+    description: 'Twogether의 이메일 찾기 페이지입니다.',
+    url: '/find-email',
+  },
+};
 
 function FindEmail() {
   return (
-    <main className="mx-4">
-      <FindEmailForm />
+    <main className="mx-4 mb-20">
+      <p className="text-center">이메일 찾기 페이지입니다.</p>
+      <p className="mb-5 text-xs text-gray-350 text-center">본인 확인을 위해 이름과 휴대폰 번호를 입력해 주세요.</p>
+      <div className="mx-4">
+        <FindEmailForm />
+      </div>
     </main>
   );
 }
