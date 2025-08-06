33744818780ccdf34c4e48b0ec8b404775df4838
apppiel <94946906+apppiel@users.noreply.github.com>
2025-08-06 13:14:56 +0900

    💄style: Add metadata to QnA search result page

--- src/app/my-page/[boardType]/search/page.tsx
+++ src/app/my-page/[boardType]/search/page.tsx
@@ -1,8 +1,32 @@
+import { SearchPageProps } from '@/app/community/[boardType]/search/page';
 import QnaSearchClient from './QnaSearchClient';
+import { Metadata } from 'next';
 
 /**
  * 1:1문의 검색 페이지 (Server Component)
  */
+
+export async function generateMetadata({ params, searchParams }: SearchPageProps): Promise<Metadata> {
+  const { boardType } = await params;
+  const { keyword } = await searchParams;
+
+  if (keyword === undefined) {
+    return {
+      title: `${boardType} : 검색 결과 -Twogether`,
+    };
+  }
+
+  return {
+    title: `${boardType} : ${keyword} 검색결과 - Twogether`,
+    description: `${boardType} : ${boardType} : ${keyword} 검색결과 입니다.`,
+    openGraph: {
+      title: `${boardType} - Twogether`,
+      description: `${boardType} 게시판`,
+      url: `/my-page/${boardType}/search?keyword=${keyword}`,
+    },
+  };
+}
+
 export default function QnaSearchPage() {
   return <QnaSearchClient />;
 }
